05c0df49fab4d921cdad9e66f654d2831d293444
贺超 <1054134542@qq.com>
2020-10-20 09:23:05 +0800

    增加objc-781版本

---
+++ objc4-781/Debug/debug-main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  HCDebug
+//
+//  Created by 贺超 on 2020/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+    }
+    return 0;
+}
